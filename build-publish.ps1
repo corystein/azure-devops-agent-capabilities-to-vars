02b08a67602cfc07a7d42d3b39b6735042c69533
Cory Stein <corystein@gmail.com>
2020-01-18 11:21:49 -0500

    Updated script

--- build-publish.ps1
+++ build-publish.ps1
@@ -4,7 +4,8 @@ Param(
     [string]$ACCOUNT = "",
     [string]$PUBLISHER = "",
     [string]$PAT,
-    [string]$EXTENSION_NAME
+    [string]$EXTENSION_NAME,
+    [switch]$PublishFromFile
 )
 
 
@@ -25,9 +26,15 @@ $ProgressPreference = "SilentlyContinue"
 if ([String]::IsNullOrWhiteSpace($(Get-Command tfx -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Definition))) {
     throw "TFX CLI is missing" 
 }
+else {
+    $(Get-Command tfx -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Definition)
+    Write-Host "Found TFX CLI"
+}
 
-if ([String]::IsNullOrWhiteSpace($(Get-Command tsc -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Definition))) {
-    throw "TypeScript CLI is missing" 
+if (-not($PublishFromFile)) {
+    if ([String]::IsNullOrWhiteSpace($(Get-Command tsc -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Definition))) {
+        throw "TypeScript CLI is missing" 
+    }
 }
 
 $WorkingDirectory = Resolve-Path -Path $WorkingDirectory
@@ -46,36 +53,80 @@ if ($Publish) {
     Write-Host "Publisher: ..................... [$PUBLISHER]"    
     Write-Host "Extension Name: ................ [$EXTENSION_NAME]"
 }
+Write-Host "Publish From File: ............. [$PublishFromFile]"
 #####################################################
 
 try {
 
     Push-Location -Path $WorkingDirectory
 
-    # Fix for VPN SSL issues
-    #npm set strict-ssl false
+    if (-not($PublishFromFile)) {
+        Push-Location -Path "../"
 
-    $taskDir = Resolve-Path -Path "$WorkingDirectory/tasks"
-    Write-Host "Tasks Directory: [$taskDir]"
-    Get-ChildItem -Path $taskDir -Recurse -Directory -Depth 0 | ForEach-Object {
-        Write-Host "Processing task folder [$_]..."
-        Push-Location $_
-        npm install
-        Write-Host $LASTEXITCODE
-        tsc
-        Pop-Location
+        ./build.ps1
+        $tasks = @("get-build-definition-id")
+        $tasks | ForEach-Object {
+            Copy-Item -Path "*.psd1" -Destination "$WorkingDirectory/tasks/$_" -Force | Out-Null
+            Copy-Item -Path "*.psm1" -Destination "$WorkingDirectory/tasks/$_" -Force | Out-Null
     
-    }
-    Write-Host "Completed Building TypeScript"
-    tfx extension create --manifest-globs extension-manifest.json
+            switch ($_) {
+                "get-build-definition-id" {
+                    Copy-Item -Path "./Invoke-AzDO.ps1" -Destination "$WorkingDirectory/tasks/$_/powershell.ps1" -Force -Verbose
+                }
+            
+            }    
+        }
+    
+        Pop-Location
 
+        #############################################
+        # Build TypeScript tasks
+        #############################################
+        <#
+        # Fix for VPN SSL issues
+        npm set strict-ssl false
+
+        $tasks = @("generate-build-info-file")
+        $tasks | ForEach-Object {
+            if ($PSVersionTable.PSEdition -ieq "Core") {
+                Push-Location "$WorkingDirectory/tasks/$_"
+            }
+            else {
+                # Windows
+                Push-Location "$WorkingDirectory/tasks/$_"
+            }
+            npm install
+            Write-Host "Last Exit Code: $($LASTEXITCODE)"
+            tsc
+            Pop-Location
+        }
+        #>
+        #############################################
+
+        tfx extension create --manifest-globs vss-extension.json
+
+    }
+    else {
+        Write-Warning "Skipping compile of extension"
+    }
 
     if ($Publish) {
-        Write-Host "Publish Extension..."
+        if (-not($PublishFromFile)) {
+            Write-Host "Publish Extension..."
 
-        tfx extension publish --manifest-globs extension-manifest.json --share-with $ACCOUNT --service-url "https://marketplace.visualstudio.com/" --auth-type pat --token "$PAT" --publisher $PUBLISHER --no-prompt --trace-level
+            tfx extension publish --manifest-globs vss-extension.json --share-with $ACCOUNT --service-url "https://marketplace.visualstudio.com/" --auth-type pat --token "$PAT" --publisher $PUBLISHER --no-prompt --trace-level
+        }
+        else {
+            Write-Host "Publish Extension [From File]..."
+            $ExtensionFile = Get-ChildItem -Filter '*.vsix' | Select-Object -First 1
+            Write-Host "Extension File: $($ExtensionFile.FullName)"
+            Write-Host "tfx extension publish --vsix $($ExtensionFile.FullName) --share-with $ACCOUNT --service-url `"https://marketplace.visualstudio.com/`" --auth-type pat --token `"$PAT`" --publisher $PUBLISHER --no-prompt --trace-level"
+            tfx extension publish --vsix "$($ExtensionFile.FullName)" --share-with $ACCOUNT --service-url "https://marketplace.visualstudio.com/" --auth-type pat --token "$PAT" --publisher $PUBLISHER --no-prompt --trace-level
+        }
         Write-Host "Last Exit Code: $LASTEXITCODE"
+        if ($LASTEXITCODE -ne 0) { throw "Failure publishing extension" }
         Write-Host "Completed Publishing Extension"
+
 
         # Uninstall VSTS Extension
         Write-Host "Uninstalling Extension (if exists)..."
